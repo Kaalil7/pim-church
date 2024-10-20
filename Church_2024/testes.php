750ac5611911b0b41da00831e190069ad23dc572
Henrique Kalil <94183276+Kaalil7@users.noreply.github.com>
2024-10-20 19:06:33 -0300

    Add files via upload

---
+++ Church_2024/testes.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste Bootstrap e Font Awesome</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha384-WvsdP3By0DJ0HKPrmf8C9CVh09zqHhP+4RfW5xLgC8vMNv6TB5f5YjCoLlJ2T4Lz" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h2>Siga-nos</h2>
+        <ul class="list-unstyled d-flex flex-row justify-content-center">
+            <li>
+                <a href="#!">
+                    <i class="fab fa-facebook-square fa-2x"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#!">
+                    <i class="fab fa-instagram fa-2x"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#!">
+                    <i class="fab fa-youtube fa-2x"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
